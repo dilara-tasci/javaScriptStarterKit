de065a13e7a4c9e893202c66a307dba0f6a0f73f
dilara-tasci <73749088+dilara-tasci@users.noreply.github.com>
2021-11-02 00:27:11 +0300

    2 Kasım Ders Sonu

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // console.log("Merhaba Kodlama.io")
 
 //JS type safe değildir
@@ -12,10 +13,30 @@ dolarDun = "Ankara"
 const euroDun = 11.20
 // euroDun = 11.10 //const'a yeniden atama yapılmaz
 // console.log(euroDun)
+=======
+console.log("Merhaba Kodlama.io")
+
+//JS type safe değildir
+let dolarBugun = 9.30
+
+let dolarDun = 9.20
+dolarDun = "9.20"
+{
+    let dolarDun = 9.10
+}
+
+console.log(dolarDun)
+
+const euroDun = 11.2
+//euroDun = 11
+
+console.log(euroDun)
+>>>>>>> 913ee0044458ba0401b6785ff179f907b3fb0c1e
 
 //array
 //camelCasing
 //PascalCasing
+<<<<<<< HEAD
 let konutKredileri = ["Konut Kredisi", "Emlak Konut Kredisi", "Kamu Konut Kredisi", "Özel Konut Kredisi"]
 let konutKredileri2 = [12, "Emlak Konut Kredisi", ["a","b","c"]]
 
@@ -28,3 +49,20 @@ let konutKredileri2 = [12, "Emlak Konut Kredisi", ["a","b","c"]]
 // console.log(konutKredileri)
 // console.log(konutKredileri2)
 
+=======
+let konutKredileri = ["Konut kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
+
+console.log("<ul>")
+for(let i = 0;i<konutKredileri.length;i++){
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
+
+{/* <ul>
+    <li>Konut kredisi</li>
+    <li>Emlak Konut Kredisi</li>
+    <li>Kamu Konut Kredisi</li>
+</ul> */}
+
+console.log(konutKredileri)
+>>>>>>> 913ee0044458ba0401b6785ff179f907b3fb0c1e
--- src/functions.js
+++ src/functions.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function addToCart(quantity, productName = "Elma"){
     console.log("Sepete Eklendi ürün: " + productName + " adet: " + quantity);
 }
@@ -8,10 +9,24 @@ addToCart(15)
 
 let sayHello =  () => {
     console.log("Hello World!");
+=======
+function addToCart(quantity,productName="Elma") {
+    console.log("Sepete eklendi : ürün : " 
+    + productName + " adet : " + quantity)
+}
+
+//addToCart()
+addToCart(10)
+//addToCart(15)
+
+let sayHello = ()=>{
+    console.log("Hello World!")
+>>>>>>> 913ee0044458ba0401b6785ff179f907b3fb0c1e
 }
 
 sayHello()
 
+<<<<<<< HEAD
 let sayHello2 = function() {
     console.log("Hello World 2");
 }
@@ -25,21 +40,49 @@ function addToCart2(productName, quantity, unitPrice) {
 addToCart2("Elma", 5, 10)
 addToCart2("Armut", 2, 20)
 addToCart2("Limon", 3, 15)
+=======
+let sayHello2 = function () {
+    console.log("Hello World 2")
+}
+
+sayHello2();
+
+function addToCart2(productName, quantity, unitPrice) {
+    
+}
+
+addToCart2("Elma",5,10)
+addToCart2("Armut", 2, 20)
+addToCart2("Limon",3,15)
+>>>>>>> 913ee0044458ba0401b6785ff179f907b3fb0c1e
 
 let product1 = {productName:"Elma", unitPrice:10, quantity:5}
 
 function addToCart3(product) {
+<<<<<<< HEAD
     console.log("Ürün: " + product.productName)
     console.log("Adet: " + product.quantity)
     console.log("Fiyat: " + product.unitPrice)
 }
 
+=======
+    console.log("Ürün : "+product.productName)
+    console.log("Adet : "+product.quantity)
+    console.log("Fiyat : "+product.unitPrice)
+}
+
+
+>>>>>>> 913ee0044458ba0401b6785ff179f907b3fb0c1e
 addToCart3(product1)
 
 let product2 = {productName:"Elma", unitPrice:10, quantity:5}
 let product3 = {productName:"Elma", unitPrice:10, quantity:5}
 product2 = product3
+<<<<<<< HEAD
 product2.productName = "Karpuz"
+=======
+product2.productName = "KARPUZ"
+>>>>>>> 913ee0044458ba0401b6785ff179f907b3fb0c1e
 console.log(product3.productName)
 
 let sayi1 = 10
@@ -51,7 +94,10 @@ console.log(sayi1)
 function addToCart4(products) {
     console.log(products)
 }
+<<<<<<< HEAD
 //eğer kendi scope'unda bulamazsa bir üst scope'a bakar
+=======
+>>>>>>> 913ee0044458ba0401b6785ff179f907b3fb0c1e
 
 let products = [
     {productName:"Elma", unitPrice:10, quantity:5},
@@ -61,6 +107,7 @@ let products = [
 
 addToCart4(products)
 
+<<<<<<< HEAD
 function add(...numbers) { //rest
     let total = 0
     for (let i = 0; i < numbers.length; i++) {
@@ -72,11 +119,16 @@ function add(...numbers) { //rest
 //rest yazımında dikkat edilmesi gereken nokta her zaman en sona bırakılması gerektiğidir. 
 /*function add(...numbers, bisey) { //rest
     let total = 0
+=======
+function add(bisey,...numbers) { //rest
+    let total = 0;
+>>>>>>> 913ee0044458ba0401b6785ff179f907b3fb0c1e
     for (let i = 0; i < numbers.length; i++) {
         total = total + numbers[i]
     }
     console.log(total)
     console.log(bisey)
+<<<<<<< HEAD
 }*/ //hatalı kullanım
 
 /*function add(bisey, ...numbers) { //rest
@@ -112,14 +164,46 @@ let [icAnadolu, marmara, karadeniz, [icAnadoluSehirleri]] =[
         ["Sinop", "Trabzon"],
     ]
 ]
+=======
+}
+
+add(20,30)
+//add(20,30, 40)
+//add(20,30, 40, 50)
+
+let numbers = [30,10,500,600,120]
+//console.log(...numbers)
+console.log(Math.max(...numbers))
+
+let [icAnadolu,marmara,karadeniz,[icAnadoluSehirleri]] = [
+    {name:"İç Anadolu", population:"20M"},
+    {name:"Marmara", population:"30M"},
+    {name:"Karadeniz", population:"10M"},
+    [
+        ["Ankara","Konya"],
+        ["İstanbul","Bursa"],
+        ["Sinop","Trabzon"],
+    ]
+]
+
+>>>>>>> 913ee0044458ba0401b6785ff179f907b3fb0c1e
 // console.log(icAnadolu.name)
 // console.log(icAnadolu.population)
 console.log(icAnadoluSehirleri)
 
 let newProductName, newUnitPrice, newQuantity
+<<<<<<< HEAD
 ({productName: newProductName, unitPrice: newUnitPrice, quantity: newQuantity} 
+=======
+({productName:newProductName, unitPrice:newUnitPrice, quantity:newQuantity } 
+>>>>>>> 913ee0044458ba0401b6785ff179f907b3fb0c1e
 = {productName:"Elma", unitPrice:10, quantity:5})
 
 console.log(newProductName)
 console.log(newUnitPrice)
+<<<<<<< HEAD
 console.log(newQuantity)
+=======
+console.log(newQuantity)
+
+>>>>>>> 913ee0044458ba0401b6785ff179f907b3fb0c1e
